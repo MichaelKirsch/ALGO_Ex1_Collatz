21efbb8e05ae3852bcf86e2c94065adf9e30956b
Michael <michael_kirsch>
2020-05-02 18:12:22 +0200

    added forced termination

--- main.cpp
+++ main.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 
-void collatz(int start)
+void collatz(int& c,int start)
 {
     std::cout << "Number:" << start << std::endl;
-    if(start<=0||start==1)
+    if(start<=1)
+    {
+        std::cout << "Done, took: "<< c <<" Steps" << std::endl;
         return;
+    }
+
+    c++;
+
+    if(c>1000)
+    {
+        std::cout <<"Forced Termination" << start << std::endl;
+        return;//forced termination
+    }
+
 
     if(start%2)
-        collatz(start/2);
+        collatz(c,start/2);
     else
-        collatz(3*start+1);
+        collatz(c,3*start+1);
 }
 
 int main() {
-    collatz(1000);
-    std::cout << "Done" << std::endl;
+    int counter =0;
+    collatz(counter,27);
+    counter=0;
+    collatz(counter,37);
+    counter =0;
+    collatz(counter,42);
     return 0;
 }
